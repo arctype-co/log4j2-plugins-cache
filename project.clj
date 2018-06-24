28364499afbc60c4d378c442301bb25d649d4246
Ryan Sundberg <ryan.sundberg@gmail.com>
2018-06-23 20:02:37 -0700

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject arctype/log4j2-plugins-cache "0.1.0-SNAPSHOT"
+(defproject arctype/log4j2-plugins-cache "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
